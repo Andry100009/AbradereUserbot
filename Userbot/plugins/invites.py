de625fffda7da214ce2c93238d1338c18dad50f3
Andrea <71290269+Andry100009@users.noreply.github.com>
2020-09-24 14:20:22 +0200

    Add files via upload

---
+++ Userbot/plugins/invites.py
@@ -0,0 +1,73 @@
+"""Invite the user(s) to the current chat
+Syntax: .invite <User(s)>"""
+
+from telethon import functions
+from userbot.system import dev_cmd
+
+
+@bot.on(dev_cmd(pattern="invite ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    to_add_users = event.pattern_match.group(1)
+    if event.is_private:
+        await event.edit("Sei scemo porco dio? esegui il comando `.invite` in un gruppo non in chat privata coglione.")
+    else:
+        logger.info(to_add_users)
+        if not event.is_channel and event.is_group:
+            # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
+            for user_id in to_add_users.split(" "):
+                try:
+                    await bot(functions.messages.AddChatUserRequest(
+                        chat_id=event.chat_id,
+                        user_id=user_id,
+                        fwd_limit=1000000
+                    ))
+                except Exception as e:
+                    await event.reply(str(e))
+            await event.edit("Aggiunto correttamente! ")
+        else:
+            # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
+            for user_id in to_add_users.split(" "):
+                try:
+                    await bot(functions.channels.InviteToChannelRequest(
+                        channel=event.chat_id,
+                        users=[user_id]
+                    ))
+                except Exception as e:
+                    await event.reply(str(e))
+            await event.edit("Aggiunto correttamente! ")
+            
+            
+
+async def _(event):
+    if event.fwd_from:
+        return
+    to_add_users = event.pattern_match.group(1)
+    if event.is_private:
+        await event.edit("Sei scemo porco dio? esegui il comando `.invite` in un gruppo non in chat privata coglione.")
+    else:
+        logger.info(to_add_users)
+        if not event.is_channel and event.is_group:
+            # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
+            for user_id in to_add_users.split(" "):
+                try:
+                    await bot(functions.messages.AddChatUserRequest(
+                        chat_id=event.chat_id,
+                        user_id=user_id,
+                        fwd_limit=1000000
+                    ))
+                except Exception as e:
+                    await event.reply(str(e))
+            await event.reply("Aggiunto correttamente! ")
+        else:
+            # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
+            for user_id in to_add_users.split(" "):
+                try:
+                    await bot(functions.channels.InviteToChannelRequest(
+                        channel=event.chat_id,
+                        users=[user_id]
+                    ))
+                except Exception as e:
+                    await event.reply(str(e))
+            await event.reply("Aggiunto correttamente! ")
